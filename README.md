c0d7a596cf49694ac13371196039713a62af5ece
jimmyma <jimmyma@croxlink.com>
2018-09-12 15:38:46 +0800

    新增註解和README

---
+++ README.md
@@ -0,0 +1,16 @@
+## get-started 裡推薦完成的課題
+### Homework
+Here are some ideas to improve the application:
+
+* Broadcast a message to connected users when someone connects or disconnects
+* Add support for nicknames
+* Don’t send the same message to the user that sent it himself. Instead, append the message directly as soon as he presses enter.
+* Add “{user} is typing” functionality
+* Show who’s online
+* Add private messaging
+* Share your improvements!
+
+
+## 參考資料:
+* [socket.io get-started](https://socket.io/get-started/chat/)
+* [MDN WebSockets](https://developer.mozilla.org/zh-TW/docs/WebSockets)
--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 var app = require('express')();
 var http = require('http').Server(app);
+// 此io帶入http server當作參數
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
+// 監聽connection事件，觸發函式
 io.on('connection', function(socket){
 	console.log('a user connected');
+
+	// 監聽disconnect事件，如果頁面關閉或刷新就會觸發函式
 	socket.on('disconnect', function(){
 		console.log('user disconnected');
 	});
 
+	// 監聽chat message事件，觸發函式
 	socket.on('chat message', function(msg){
+		// 發出chat message事件到所有瀏覽器端使用者
 		io.emit('chat message', msg);
 		console.log('message: ' + msg);
 	});
